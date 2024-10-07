0640e7043eed675fb2c4ef7f62e2c57cc6d100f9
unknown <patricolino@gmail.com>
2024-10-07 20:05:21 +0200

    Fixed and reimplemented gender_sort and manage_playlist, now app working 100%

--- app/models/PlaylistManager.py
+++ app/models/PlaylistManager.py
@@ -1,4 +1,5 @@
 # OPTIMIZAR PARA USAR MENOS O COMMIT TROCAR SISTEMA EM VEZ DE NUMERO DE MUSICAS POR NUMERO DE VOTOS 
+import math
 class PlaylistManager:
     def __init__(self, spotify_instace, db_instance, playlist_id, playlist_limit):
         """
@@ -84,19 +85,20 @@ class PlaylistManager:
             # Handle case when all tracks have same amount of votes
             return []
         else:
+            # Here i might need to select only the id directly, dunnow
             self._cursor.execute("SELECT * FROM tracks WHERE votos > (SELECT MIN(votos) FROM tracks) ORDER BY votos DESC LIMIT %s", (self.playlist_limit,))
             favorite_tracks = self._cursor.fetchall()
             return [track[0] for track in favorite_tracks]
     def get_db_genres_proportions_list(self):
         """
-        Get a descending list with the name of the genres and their respective proportions in the database
+        Get a descending list with the name of the genres and their respective vote proportions in the database
 
         returns
         ----------
         Descending list with the name of the genres and their respective proportions in the database in form of tuples (genre, proportion)
         ----------
         """
-        self._cursor.execute("SELECT genre, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM tracks) AS proportion FROM tracks GROUP BY genre ORDER BY proportion DESC")
+        self._cursor.execute("SELECT genre, SUM(votos) / (SELECT SUM(votos) FROM tracks) AS proportion FROM tracks GROUP BY genre ORDER BY proportion DESC")
         result = self._cursor.fetchall()
         print(result)
         return result
@@ -122,6 +124,7 @@ class PlaylistManager:
             uri of the track to add
         ----------
         """
+        self._cursor.execute("UPDATE tracks SET in_playlist = 1 WHERE id = %s", (track_uri,))
         self.spotify.playlist_add_items(self.playlist_id, [track_uri])
 
     def add_track_to_db(self, id, name, artist, genre):
@@ -197,7 +200,6 @@ class PlaylistManager:
             genre = artist_info['genres'][0] if artist_info.get('genres') else 'Unknown'
             # Add track to the database and the Spotify playlist
             self.add_track_to_db(track_uri, name, artist, genre)  
-            self._cursor.execute("UPDATE tracks SET in_playlist = 1 WHERE id = %s", (track_uri,))
             if not self.is_inside_playlist(track_uri):    
                 self.add_track_to_playlist(track_uri)
         
@@ -206,7 +208,7 @@ class PlaylistManager:
     
     def remove_track_general(self, track_uri):
         """
-        Remove track from playlist and database
+        Remove track from playlist and update its status in the database
 
         Parameters
         ----------  
@@ -220,60 +222,88 @@ class PlaylistManager:
         
     # Possible improve by also sorting by artist
     def gender_sort(self):
-        
-        genre_proportions = self.get_db_genres_proportions_list()
-        big_genders = sum(1 for genre, proportion in genre_proportions if proportion >= 0.1)
+        """
+        Tool to sort the playlist, selecting favorite tracks and when needed selecting tracks to fill the playlist according to their genders
+
+        Returns
+        ---------
+        Set containing track's uris to be added in the playlist
+        ---------
+        """
         favorite_tracks = self.get_favorite_tracks()
-        size_to_sort = self.playlist_limit - len(favorite_tracks)
-        tracks_to_add = []
+        print("FAVORITE TRACKS ARE: ")
+        print(favorite_tracks)
+        tracks_to_add = set()
 
-        index = 0
-        limit = size_to_sort
-        
-        # Add tracks from big genders
-        for i in range(big_genders):
-            genre, proportion = genre_proportions[i]
-            amount_to_add = min(int(round(proportion * size_to_sort)), limit)
-            
-            self._cursor.execute("SELECT id FROM tracks WHERE genre = %s ORDER BY votos DESC LIMIT %s", 
-                                (genre, amount_to_add))
-            tracks = self._cursor.fetchall()
-            
-            for track in tracks:
-                if track[0] not in tracks_to_add and track[0] not in favorite_tracks:
-                    tracks_to_add.append(track[0])
-                    
-            limit = size_to_sort - len(tracks_to_add)  # Update limit after adding tracks
-        
-        # If there's still space, fill with remaining tracks regardless of genre
-        while limit > 0 and index < len(genre_proportions):
-            genre, _ = genre_proportions[index]
-            
-            # MAYBE ADD ASC INSTEAD OF DESC
-            self._cursor.execute("SELECT id FROM tracks WHERE genre = %s ORDER BY votos DESC LIMIT %s", 
-                                (genre, limit))
-            tracks = self._cursor.fetchall()
-            
-            for track in tracks:
-                if track[0] not in tracks_to_add:
-                    tracks_to_add.append(track[0])
-            
-            limit = size_to_sort - len(tracks_to_add)  # Recalculate limit after each addition
-            index += 1
-        
-        # Add favorite tracks at the end, if not already in the list
-        for track in favorite_tracks:
-            if track not in tracks_to_add:
-                tracks_to_add.append(track)
-        print("TRACKS TO ADD INSIDE OF GENDER SORT")    
+        # Add favorite tracks to the set
+        tracks_to_add.update(favorite_tracks)
+        print("First update of tracks_to_add")
         print(tracks_to_add)
-        return tracks_to_add
-
-                
-
         
-    # Redundancy calling favorite_tracks in both methods ^ and down
+        size_to_sort = self.playlist_limit - len(tracks_to_add)
+        # case when there isnt enough favorite tracks to fill the playlist
+        if size_to_sort > 0:
+            gender_info = self.get_db_genres_proportions_list()
+            print("Gender_info")
+            print(gender_info)
+            big_genders = sum(1 for genre, proportion in gender_info if proportion >= 0.1)
+            print("There are "+str(big_genders)+" big genders")
 
+            # avoid fetching favorites from the database as they were already added 
+            if tracks_to_add:
+                not_in_clause = " AND id NOT IN (" + ', '.join(['%s'] * len(tracks_to_add)) + ")"
+                print("not_in_clause was prepared and is "+not_in_clause)
+            else: 
+                not_in_clause = ""
+                print("not prepared")
+            print("Size to sort is "+str(size_to_sort))
+            # insert tracks from big genders
+            for i in range(big_genders):
+                print(i)
+                genre, proportion = gender_info[i]
+                amount_to_add = int(math.floor(proportion * size_to_sort))
+                print("Going to add "+str(amount_to_add)+"musics from gender "+str(genre))
+
+                query = "SELECT id FROM tracks WHERE genre = %s" + not_in_clause + " ORDER BY votos DESC LIMIT %s"
+                args = [genre] + list(favorite_tracks) + [amount_to_add]
+                print("Prepared query 1:", query)
+                print("With args 1:", args)
+                self._cursor.execute(query, tuple(args))
+                tracks = self._cursor.fetchall()
+                print("tracks to add are: ")
+                print(tracks)
+                tracks_to_add.update(track[0] for track in tracks)
+            
+            limit = self.playlist_limit - len(tracks_to_add)
+            print("Limit is : "+ str(limit))
+
+            if limit > 0:
+                not_in_clause = " id NOT IN (" + ', '.join(['%s'] * len(tracks_to_add)) + ")"
+                
+                query = f"""
+                SELECT t.id, t.genre 
+                FROM tracks AS t 
+                JOIN (
+                SELECT genre, SUM(votos) AS gen_sum 
+                FROM tracks 
+                WHERE {not_in_clause} 
+                GROUP BY genre 
+                ORDER BY gen_sum DESC
+                ) AS g ON t.genre = g.genre 
+                ORDER BY g.gen_sum DESC, g.genre
+                LIMIT %s;
+                """
+                print("not_in_clasue "+not_in_clause)
+                args = list(tracks_to_add) + [limit]
+                print("args : "+str(args))
+
+                self._cursor.execute(query, tuple(args))
+                tracks = self._cursor.fetchall()
+                print("tracks_to_add are")
+                tracks_to_add.update(track[0] for track in tracks)
+
+        return tracks_to_add
+    
     def manage_playlist(self):
         """
         ------------
@@ -281,46 +311,30 @@ class PlaylistManager:
         Adds the top-voted tracks and fills remaining slots based on genre proportions.
         
         """
-        
-    
-        total_tracks_in_db = self.get_count_tracks_db()
-        
         # Case when playlist has not yet reached its limit
-        if (total_tracks_in_db < self.playlist_limit):
-            return "Playlist not full"
-        
-        current_tracks = self.get_playlist_tracks()
-        tracks_to_add = self.get_favorite_tracks()
-        print("Favorite tracks inside of manage_playlist")
-        print(tracks_to_add)
-        # Case when i have enough favorite tracks to fill the playlist 
-        if (len(tracks_to_add) == self.playlist_limit):
-            for current in current_tracks:
-                if current not in tracks_to_add:
-                    self.remove_track_general(current)
-                else:
-                    tracks_to_add.remove(current)   
-            for track in tracks_to_add:
-                self.add_track_general(track)
-            return "Playlist filled with favorite tracks"
-
-        # Case when i don't have enough favorite tracks to fill the playlist, mix input of favorites plus gender_sort
-        else:
-            print("Case when do not have enough favorites")
-            new_tracks = self.gender_sort()
-            for current in current_tracks:
-                if current not in new_tracks:
-                    self.remove_track_general(current)
-                else:
-                    new_tracks.remove(current)
-            for track in new_tracks:
-                self.add_track_general(track)
-            return "Playlist filled with favorite if there are and gender sorted"
-
+        if self.get_count_tracks_db() < self.playlist_limit:
+            return "Nothing was done since not enough tracks to fill the playlist"
         
 
-    
-            
+        add_set = self.gender_sort()
+        print("INSIDE OF MANAGE_PLAYLIST")
+        print("add set")
+        print(add_set)
+        current_tracks = set(self.get_playlist_tracks())
+        print("Current tracks")
+        print(current_tracks)
+       
 
+        for track in current_tracks - add_set:
+            print("removing FINAL")
+            print(track)
+            self.remove_track_general(track)
+
+        # Add new tracks to the playlist
+        for track in add_set - current_tracks:
+            print("ADDING FINAL")
+            print(track)
+            self.add_track_to_playlist(track)
         
-        
+
+        return "Finished managing the playlist"
